6a21214db51e8e27b003bccfc41774b098222d96
abel <abeltb97@gmail.com>
2021-09-20 14:14:37 +0200

    Segunda Version

--- src/main/java/com/everis/mvniniciales/App.java
+++ src/main/java/com/everis/mvniniciales/App.java
@@ -7,6 +7,7 @@ import org.apache.commons.lang3.StringUtils;
  */
 public class App 
 {
+	// Abel Trujillo
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
